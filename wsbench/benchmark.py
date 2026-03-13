4e3034a4ba571f5c0436a52cb8b0f74505e8e9b7
taras <nomail@nomail>
2026-03-13 06:31:09 +0100

    Update benchmark

--- wsbench/benchmark.py
+++ wsbench/benchmark.py
@@ -37,6 +37,7 @@ def print_result_and_plot(msg_size, results: pd.DataFrame, save_plot):
         "websockets": "orange",
         "aiohttp": "green",
         "picows": "red",
+        "picows_aiofastnet": "darkred",
         "picows_cyt": "darkred",
         "boost": "black"
     }
@@ -97,10 +98,10 @@ def main():
     parser = argparse.ArgumentParser(description="Benchmark for the various websocket clients",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--host", default="127.0.0.1", help="Server host")
-    parser.add_argument("--tcp-port", default="9001", help="Server port with plain tcp websockets")
-    parser.add_argument("--ssl-port", default="9002", help="Server port with ssl websockets")
-    parser.add_argument("--msg-size", default="256", help="Message size")
-    parser.add_argument("--duration", default="5", help="duration of test in seconds")
+    parser.add_argument("--tcp-port", type=int, default="9001", help="Server port with plain tcp websockets")
+    parser.add_argument("--ssl-port", type=int, default="9002", help="Server port with ssl websockets")
+    parser.add_argument("--msg-size", type=int, default="256", help="Message size")
+    parser.add_argument("--duration", type=int, default="5", help="duration of test in seconds")
     if os.name != 'nt':
         parser.add_argument("--loops", default="asyncio,uvloop", help="Comma separated list of event loops")
     else:
@@ -109,7 +110,7 @@ def main():
     parser.add_argument("--save-plot", action="store_true", help="Save plot to results folder instead of showing them")
 
     parser.add_argument("--clients",
-                        default="tornado,ws4py,websockets,aiohttp,picows,boost",
+                        default="tornado,ws4py,websockets,aiohttp,picows,picows_aiofastnet,boost",
                         help="Comma separated list of clients")
     parser.add_argument("--skip-tcp", action="store_true", help="Disable plain tcp client test")
     parser.add_argument("--skip-ssl", action="store_true", help="Disable ssl client test")
@@ -118,12 +119,9 @@ def main():
 
     args = parser.parse_args()
 
-    msg_size = int(args.msg_size)
     loops = args.loops.split(",")
     pd_index = (args.clients.split(","))
     modules = (f"wsbench.client_{c}" for c in pd_index)
-
-    duration = int(args.duration)
 
     ssl_context = create_client_ssl_context()
     tcp_url = f"ws://{args.host}:{args.tcp_port}/"
@@ -149,7 +147,7 @@ def main():
         if module_idx == 0:
             pd_columns.append("version")
         for ctx, url in tcp_ssl_targets:
-            msg = os.urandom(msg_size)
+            msg = os.urandom(args.msg_size)
 
             if m.name not in ('c++ beast', 'ws4py'):
                 for loop in loops:
@@ -163,16 +161,16 @@ def main():
                         asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
 
                     tcp_ssl_name = 'tcp' if ctx is None else 'ssl'
-                    print(f"Run {m.name} {tcp_ssl_name} {msg_size} bytes {loop} test")
-                    rps = asyncio.run(m.run(args, url, msg, duration, 100, ctx))
+                    print(f"Run {m.name} {tcp_ssl_name} {args.msg_size} bytes {loop} test")
+                    rps = asyncio.run(m.run(args, url, msg, args.duration, 100, ctx))
 
                     if module_idx == 0:
                         pd_columns.append(f"{tcp_ssl_name}-{loop}")
                     module_results.append(rps)
             else:
                 tcp_ssl_name = 'tcp' if ctx is None else 'ssl'
-                print(f"Run {m.name} {tcp_ssl_name} {msg_size} bytes test")
-                rps = asyncio.run(m.run(args, url, msg, duration, 100, ctx))
+                print(f"Run {m.name} {tcp_ssl_name} {args.msg_size} bytes test")
+                rps = asyncio.run(m.run(args, url, msg, args.duration, 100, ctx))
 
                 for loop in loops:
                     if module_idx == 0:
@@ -186,7 +184,7 @@ def main():
 
     df = pd.DataFrame(results, index=pd_index, columns=pd_columns)
     if not args.no_plot:
-        print_result_and_plot(msg_size, df, args.save_plot)
+        print_result_and_plot(args.msg_size, df, args.save_plot)
 
 
 if __name__ == '__main__':
---
+++ wsbench/picows_listener.py
@@ -0,0 +1,48 @@
+from time import time
+from typing import Optional
+
+from picows import WSListener, WSTransport, WSMsgType, WSFrame
+
+
+class EchoClientListener(WSListener):
+    _transport: Optional[WSTransport]
+    _start_time: float
+    _duration: float
+    _warmup_cycles_cnt: int
+    _cnt: int
+    _data: bytes
+
+    rps: int
+
+    def __init__(self, data, duration, warmup_cycles_cnt):
+        super().__init__()
+        self._transport = None
+        self._start_time = 0
+        self._duration = duration
+        self._warmup_cycles_cnt = warmup_cycles_cnt
+        self._cnt = 0
+        self._data = data
+        self.rps = 0
+
+    def on_ws_connected(self, transport: WSTransport):
+        self._transport = transport
+        if self._warmup_cycles_cnt == 0:
+            self._start_time = time()
+        self._transport.send(WSMsgType.BINARY, self._data)
+
+    def on_ws_frame(self, transport: WSTransport, frame: WSFrame):
+        now = time()
+
+        if self._warmup_cycles_cnt > 0:
+            self._warmup_cycles_cnt -= 1
+            if self._warmup_cycles_cnt == 0:
+                self._start_time = now
+        else:
+            self._cnt += 1
+
+            if now - self._start_time >= self._duration:
+                self.rps = int(self._cnt / self._duration)
+                self._transport.disconnect()
+                return
+
+        self._transport.send(WSMsgType.BINARY, self._data)
