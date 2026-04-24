9b1da9a6954c108b2012950756f0904884cc3678
taras <nomail@nomail>
2026-04-25 00:29:41 +0200

    Add isolated example

---
+++ wsbench/rsloop_websockets.py
@@ -0,0 +1,23 @@
+import logging
+
+import rsloop
+from websockets import connect
+
+
+HOST = "127.0.0.1"
+PORT = 9001
+URL = f"ws://{HOST}:{PORT}/"
+
+
+async def main():
+    async with connect(URL, compression=None, ping_interval=None) as websocket:
+        for idx in range(10):
+            message = f"hello {idx}"
+            await websocket.send(message)
+            reply = await websocket.recv()
+            print(f"sent: {message} received: {reply}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    rsloop.run(main(), debug=True)
